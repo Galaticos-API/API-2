02778b52832404b9c948939be966a5dba3b57c1d
Rafael Matesco <matescorapha@gmail.com>
2025-09-08 21:49:26 -0300

    feat - project start

--- code/src/main/java/com/youtan/pdi/MainApplication.java
+++ code/src/main/java/com/youtan/pdi/MainApplication.java
@@ -7,13 +7,24 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class LoginApplication extends Application {
+public class MainApplication extends Application {
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(LoginApplication.class.getResource("Login.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        // Carrega o arquivo FXML da tela de login
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("Login.fxml"));
+
+        // Cria a cena com base no arquivo FXML
+        Scene scene = new Scene(fxmlLoader.load(), 400, 400);
+
+        // Configura e exibe a janela
+        stage.setTitle("Sistema PDI - Login");
         stage.setScene(scene);
+        stage.setMaximized(true);
         stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
     }
 }
