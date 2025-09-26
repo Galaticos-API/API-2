a38a8d2bf21ff48e0d69918c4e8309853fbb4051
Rafael Matesco <matescorapha@gmail.com>
2025-09-26 15:26:34 -0300

    FEAT - Atualizar estrutura do banco de dados

--- DOCS/pdi_youtan.sql
+++ DOCS/pdi_youtan.sql
@@ -17,71 +17,93 @@ USE `pdi_youtan`;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-
 --
--- Table structure for table `usuario`
+-- Table structure for table `colaborador`
 --
 
-DROP TABLE IF EXISTS `usuario`;
+DROP TABLE IF EXISTS `colaborador`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `usuario` (
+CREATE TABLE `colaborador` (
   `id` int NOT NULL AUTO_INCREMENT,
   `nome` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `senha` varchar(255) NOT NULL,
-  `tipo_usuario` enum('RH','Gestor de Area','Gestor Geral','Colaborador') NOT NULL,
-  `data_criacao` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `status` enum('Ativo','Inativo') NOT NULL,
+  `cpf` varchar(14) NOT NULL,
+  `data_nascimento` date DEFAULT NULL,
+  `cargo` varchar(100) DEFAULT NULL,
+  `experiencia` varchar(255) DEFAULT NULL,
+  `observacoes` text,
+  `gerente_id` int DEFAULT NULL,
+  `usuario_id` int DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  UNIQUE KEY `usuario_id` (`usuario_id`),
+  KEY `gerente_id` (`gerente_id`),
+  CONSTRAINT `colaborador_ibfk_1` FOREIGN KEY (`gerente_id`) REFERENCES `usuario` (`id`),
+  CONSTRAINT `colaborador_ibfk_2` FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 --
--- Table structure for table `pdi`
+-- Dumping data for table `colaborador`
 --
 
-DROP TABLE IF EXISTS `pdi`;
+LOCK TABLES `colaborador` WRITE;
+/*!40000 ALTER TABLE `colaborador` DISABLE KEYS */;
+/*!40000 ALTER TABLE `colaborador` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `documento`
+--
+
+DROP TABLE IF EXISTS `documento`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `pdi` (
+CREATE TABLE `documento` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `colaborador_id` int NOT NULL,
-  `ano` int NOT NULL,
-  `status` enum('Em Andamento','Concluído','Arquivado') NOT NULL,
-  `data_criacao` date NOT NULL,
-  `data_fechamento` date DEFAULT NULL,
-  `pontuacao_geral` decimal(5,2) DEFAULT NULL,
+  `pdi_id` int NOT NULL,
+  `nome_arquivo` varchar(255) NOT NULL,
+  `caminho_arquivo` varchar(255) NOT NULL,
+  `data_upload` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `tipo_documento` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `colaborador_id` (`colaborador_id`),
-  CONSTRAINT `pdi_ibfk_1` FOREIGN KEY (`colaborador_id`) REFERENCES `colaborador` (`id`)
+  KEY `pdi_id` (`pdi_id`),
+  CONSTRAINT `documento_ibfk_1` FOREIGN KEY (`pdi_id`) REFERENCES `pdi` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
 
 --
--- Table structure for table `pdi_habilidade`
+-- Dumping data for table `documento`
 --
 
-DROP TABLE IF EXISTS `pdi_habilidade`;
+LOCK TABLES `documento` WRITE;
+/*!40000 ALTER TABLE `documento` DISABLE KEYS */;
+/*!40000 ALTER TABLE `documento` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `habilidade`
+--
+
+DROP TABLE IF EXISTS `habilidade`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `pdi_habilidade` (
-  `pdi_id` int NOT NULL,
-  `habilidade_id` int NOT NULL,
-  `nivel_avaliacao` int DEFAULT NULL,
-  `data_avaliacao` date DEFAULT NULL,
-  PRIMARY KEY (`pdi_id`,`habilidade_id`),
-  KEY `habilidade_id` (`habilidade_id`),
-  CONSTRAINT `pdi_habilidade_ibfk_1` FOREIGN KEY (`pdi_id`) REFERENCES `pdi` (`id`),
-  CONSTRAINT `pdi_habilidade_ibfk_2` FOREIGN KEY (`habilidade_id`) REFERENCES `habilidade` (`id`)
+CREATE TABLE `habilidade` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) NOT NULL,
+  `tipo_skill` enum('Hard Skill','Soft Skill') NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `nome` (`nome`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
+--
+-- Dumping data for table `habilidade`
+--
+
+LOCK TABLES `habilidade` WRITE;
+/*!40000 ALTER TABLE `habilidade` DISABLE KEYS */;
+/*!40000 ALTER TABLE `habilidade` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `objetivo`
@@ -104,72 +126,110 @@ CREATE TABLE `objetivo` (
   CONSTRAINT `objetivo_ibfk_1` FOREIGN KEY (`pdi_id`) REFERENCES `pdi` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
 
 --
--- Table structure for table `habilidade`
+-- Dumping data for table `objetivo`
 --
 
-DROP TABLE IF EXISTS `habilidade`;
+LOCK TABLES `objetivo` WRITE;
+/*!40000 ALTER TABLE `objetivo` DISABLE KEYS */;
+/*!40000 ALTER TABLE `objetivo` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `pdi`
+--
+
+DROP TABLE IF EXISTS `pdi`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `habilidade` (
+CREATE TABLE `pdi` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `nome` varchar(100) NOT NULL,
-  `tipo_skill` enum('Hard Skill','Soft Skill') NOT NULL,
+  `colaborador_id` int NOT NULL,
+  `ano` int NOT NULL,
+  `status` enum('Em Andamento','Concluído','Arquivado') NOT NULL,
+  `data_criacao` date NOT NULL,
+  `data_fechamento` date DEFAULT NULL,
+  `pontuacao_geral` decimal(5,2) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `nome` (`nome`)
+  KEY `colaborador_id` (`colaborador_id`),
+  CONSTRAINT `pdi_ibfk_1` FOREIGN KEY (`colaborador_id`) REFERENCES `colaborador` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
 
 --
--- Table structure for table `documento`
+-- Dumping data for table `pdi`
 --
 
-DROP TABLE IF EXISTS `documento`;
+LOCK TABLES `pdi` WRITE;
+/*!40000 ALTER TABLE `pdi` DISABLE KEYS */;
+/*!40000 ALTER TABLE `pdi` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `pdi_habilidade`
+--
+
+DROP TABLE IF EXISTS `pdi_habilidade`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `documento` (
-  `id` int NOT NULL AUTO_INCREMENT,
+CREATE TABLE `pdi_habilidade` (
   `pdi_id` int NOT NULL,
-  `nome_arquivo` varchar(255) NOT NULL,
-  `caminho_arquivo` varchar(255) NOT NULL,
-  `data_upload` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `tipo_documento` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `pdi_id` (`pdi_id`),
-  CONSTRAINT `documento_ibfk_1` FOREIGN KEY (`pdi_id`) REFERENCES `pdi` (`id`)
+  `habilidade_id` int NOT NULL,
+  `nivel_avaliacao` int DEFAULT NULL,
+  `data_avaliacao` date DEFAULT NULL,
+  PRIMARY KEY (`pdi_id`,`habilidade_id`),
+  KEY `habilidade_id` (`habilidade_id`),
+  CONSTRAINT `pdi_habilidade_ibfk_1` FOREIGN KEY (`pdi_id`) REFERENCES `pdi` (`id`),
+  CONSTRAINT `pdi_habilidade_ibfk_2` FOREIGN KEY (`habilidade_id`) REFERENCES `habilidade` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
 
 --
--- Table structure for table `colaborador`
+-- Dumping data for table `pdi_habilidade`
 --
 
-DROP TABLE IF EXISTS `colaborador`;
+LOCK TABLES `pdi_habilidade` WRITE;
+/*!40000 ALTER TABLE `pdi_habilidade` DISABLE KEYS */;
+/*!40000 ALTER TABLE `pdi_habilidade` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuario`
+--
+
+DROP TABLE IF EXISTS `usuario`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `colaborador` (
+CREATE TABLE `usuario` (
   `id` int NOT NULL AUTO_INCREMENT,
   `nome` varchar(255) NOT NULL,
-  `cpf` varchar(14) NOT NULL,
-  `data_nascimento` date DEFAULT NULL,
-  `cargo` varchar(100) DEFAULT NULL,
-  `experiencia` varchar(255) DEFAULT NULL,
-  `observacoes` text,
-  `gerente_id` int DEFAULT NULL,
-  `usuario_id` int DEFAULT NULL,
+  `email` varchar(255) NOT NULL,
+  `senha` varchar(255) NOT NULL,
+  `tipo_usuario` enum('RH','Gestor de Area','Gestor Geral','Colaborador') NOT NULL,
+  `data_criacao` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `status` enum('Ativo','Inativo') NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `cpf` (`cpf`),
-  UNIQUE KEY `usuario_id` (`usuario_id`),
-  KEY `gerente_id` (`gerente_id`),
-  CONSTRAINT `colaborador_ibfk_1` FOREIGN KEY (`gerente_id`) REFERENCES `usuario` (`id`),
-  CONSTRAINT `colaborador_ibfk_2` FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuario`
+--
+
+LOCK TABLES `usuario` WRITE;
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-09-26 15:25:46
