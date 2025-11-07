02ff90880360e43361327e1d1f38014b11232e90
Rafael Matesco <matescorapha@gmail.com>
2025-11-07 08:19:53 -0300

    DOCS - fix sql

--- DOCS/pdi_youtan.sql
+++ DOCS/pdi_youtan.sql
@@ -15,6 +15,9 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+CREATE DATABASE pdi_youtan;
+USE pdi_youtan;
+
 --
 -- Table structure for table `avaliacao`
 --
